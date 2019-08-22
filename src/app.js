80e00800a1adb694164b6cccda610eec4e78ff77
carolinaab <tau_karo@hotmail.com>
2019-08-22 01:30:33 -0500

    se agrega funcion de mostrar y ocultar secciones

--- src/app.js
+++ src/app.js
@@ -1,5 +1,36 @@
-let boton = document.getElementById("vengadores");
+// mostrar y ocultar
 
-let vengaTrivia = document.getElementById("vengadores-trivia");
+const hideSection = (id) => {
+  document.getElementById(id).style.display ="none";
+}
 
+const showSection = (id) => {
+    document.getElementById(id).style.display ="block";
 
+}
+// inicio 
+
+ 
+
+// seccion VENGADORES 
+const preguntasVengadores = document.getElementById("preguntas-vengadores");
+
+preguntasVengadores.addEventListener("click",vengadores);
+
+function vengadores(){
+  hideSection("elige");
+  showSection("vengadores");
+}
+ 
+//  seccion LIGA
+
+const preguntasLiga = document.getElementById("preguntas-liga");
+
+preguntasLiga.addEventListener("click",liga);
+
+function liga(){
+  hideSection("elige");
+  showSection("liga");
+}
+ 
+ 
